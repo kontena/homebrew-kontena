537c933e9fa7f300e1e0c333a5c53903f052b21e
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-11-24 16:15:44 +0200

    Stable #{ARGV[0]}

--- Formula/kontena.rb
+++ Formula/kontena.rb
@@ -5,13 +5,9 @@ class Kontena < Formula
 
   stable do
     url "https://github.com/kontena/kontena.git",
-        :tag => "v1.4.0",
-        :revision => "b9428ffc718da5427d6356705e830b127269bc6c"
+        :tag => "v1.4.1",
+        :revision => "ec9a0dad29105bedd46838bf9092bf867b4b9922"  end
 
-    resource "addressable" do
-      url "https://rubygems.org/gems/addressable-2.5.2.gem"
-      sha256 "73771ea960b3900d96e6b3729bd203e66f387d0717df83304411bf37efd7386e"
-    end
 
     resource "clamp" do
       url "https://rubygems.org/gems/clamp-1.1.2.gem"
@@ -154,12 +150,7 @@ class Kontena < Formula
     end
   end
 
-  bottle do
-    cellar :any
-    sha256 "b24b6c27bd25f03a425a29b692534fc7c81b4144fa1c6b1fd1f8fbbf2b0b9c63" => :high_sierra
-    sha256 "529128d8ff36c0afcb33b7499d0b01cef550701c24c71db6115e92cb4d75b94f" => :sierra
-    sha256 "f0f44fde8847a0e94e9a3238d9cd8af92b2cf3a171401830ff569406b753789f" => :el_capitan
-  end
+
 
   devel do
     url "https://github.com/kontena/kontena.git",
